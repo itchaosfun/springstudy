3fb60d82dd2bffe5d01e07337b977befb8d4a714
Cao Youchao <caoyouchao@cmsr.chinamobile.com>
2020-01-08 17:12:40 +0800

    first commit

---
+++ userservice/src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.example.userservice.controller;
+
+import com.example.userservice.dao.UserRepository;
+import com.example.userservice.data.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/{id}")
+    public User findById(@PathVariable Long id) {
+        System.out.println("id = " + id);
+        User user = this.userRepository.getOne(id);
+        System.out.println("user = " + user);
+        return user;
+    }
+}
