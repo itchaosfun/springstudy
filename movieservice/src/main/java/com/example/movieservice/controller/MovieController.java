8a5cd8e0570d2edd085e3b565c739a3e0293e66d
Cao Youchao <caoyouchao@cmsr.chinamobile.com>
2020-01-13 10:55:06 +0800

    新建eureka微服务

--- movieservice/src/main/java/com/example/movieservice/controller/MovieController.java
+++ movieservice/src/main/java/com/example/movieservice/controller/MovieController.java
@@ -1,8 +1,8 @@
 package com.example.movieservice.controller;
 
-import com.example.movieservice.common.Constance;
 import com.example.movieservice.data.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +10,15 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class MovieController {
+
+    @Value("${user.userservice.url}")
+    private String url;
+
     @Autowired
     private RestTemplate restTemplate;
 
     @GetMapping("/user/{id}")
     public User findById(@PathVariable Long id) {
-        return this.restTemplate.getForObject(Constance.url + id, User.class);
+        return this.restTemplate.getForObject(this.url + id, User.class);
     }
-
 }
